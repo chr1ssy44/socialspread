6ecd05e1d0c049022a50f8f17e8119e5b7e46813
chr1ssy44 <cbeckford2@brooklynsteamcenter.org>
2024-02-07 10:48:41 -0500

    added a file

--- main.py
+++ main.py
@@ -1,10 +1,46 @@
 from flask import Flask, render_template, request, redirect
+import flask_login
 import pymysql
 import pymysql.cursors
 from flask_httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
+app.secret_key = "top_secret"
+
+login_manager =flask_login.LoginManager()
+
+login_manager.init_app(app)
+
 auth = HTTPBasicAuth()
+
+class User:
+    is_authenticated = True
+    is_anonymous = False
+    is_active = True
+
+    def __init__(self, id, username, password):
+        self.username = username
+        self.id = id
+        self.password = password
+
+    def get_id(self):
+
+        return str(self.id) 
+    
+@login_manager.user_loader
+def load_user(user_id):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * from `user` WHERE `id` = " + str(user_id))
+    result = cursor.fetchone()
+    cursor.close()
+    conn.commit()
+
+    if result is None:
+        
+        return None
+    
+    return User(result['id'], result['username'], result['password'])
+
 
 conn = pymysql.connect(
     database = "cbeckford2_socialspread",
@@ -16,23 +52,51 @@ conn = pymysql.connect(
 
 @app.route('/')
 def index():
+
     return render_template('home.html.jinja')
 
 @app.route('/registration', methods = ['GET','POST'])
 def registration_page():
     if request.method == 'POST':
-        first_name = request.form["first name"]
-        last_name = request.form["last name"]
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
         username = request.form["username"]
         password = request.form["password"]
-        email = request.form["email"]
 
         cursor = conn.cursor()
-        cursor.execute(f"INSERT INTO `users` (__PUT_COLUMNS_HERE__) VALUES ('{username}', '{password}', '{email}', {first_name}, {last_name})")
+        cursor.execute(f"INSERT INTO `user` (`first_name`, `last_name`, `username`, `password`) VALUES ('{first_name}', '{last_name}', '{username}', '{password}')")
         cursor.close()
         conn.commit()
         
-    return render_template('registration.html.jinja')
+    return render_template('register.html.jinja')
 
 if __name__ == '__main__':
     app.run()
+
+@app.route('/login', methods = ['GET','POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form["username"]
+        password = request.form["password"]
+        cursor = conn.cursor()
+        cursor.execute(f"SELECT * FROM `user` WHERE `username` = '{username}'")
+        user = cursor.fetchone()
+        cursor.close()
+        conn.commit()
+
+        if request.form['password'] == user['password']:
+            user = load_user(user['id'])
+
+            flask_login.login_user(user)
+
+            return redirect ('/feed')
+    
+    return render_template('login.html.jinja')
+
+@app.route('/feed', methods = ['GET', 'POST'])
+@flask_login.login_required
+def post_feed():
+
+    return render_template('feed.html.jinja')
+
+ 
