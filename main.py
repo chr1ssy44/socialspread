43232882aa2133777c13b07858d713327858562e
chr1ssy44 <cbeckford2@brooklynsteamcenter.org>
2024-02-14 09:54:22 -0500

    added a file

--- main.py
+++ main.py
@@ -95,12 +95,13 @@ def login():
     
     return render_template('login.html.jinja')
 
-posts =['']
+post =['']
 
 @app.route('/feed', methods = ['GET', 'POST'])
 @flask_login.login_required
 def post_feed():
-    return render_template('feed.html.jinja')
+    
+    return render_template('feed.html.jinja', my_posts=post)
 
     return flask_login.current_user
 
