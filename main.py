73b22d4166fa5222ca3bc7bd3128a6756ccb1565
chr1ssy44 <cbeckford2@brooklynsteamcenter.org>
2024-02-15 10:52:57 -0500

    added a file

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, g
 import flask_login
 import pymysql
 import pymysql.cursors
@@ -29,11 +29,11 @@ class User:
     
 @login_manager.user_loader
 def load_user(user_id):
-    cursor = conn.cursor()
+    cursor = get_db().cursor()
     cursor.execute("SELECT * from `user` WHERE `id` = " + str(user_id))
     result = cursor.fetchone()
     cursor.close()
-    conn.commit()
+    get_db().commit()
 
     if result is None:
         
@@ -42,14 +42,27 @@ def load_user(user_id):
     return User(result['id'], result['username'], result['password'])
 
 
-conn = pymysql.connect(
-    database = "cbeckford2_socialspread",
-    user = "cbeckford2",
-    password = "227248309",
-    host = "10.100.33.60",
-    cursorclass = pymysql.cursors.DictCursor
-)
+def connect_db():
+    return pymysql.connect(
+        host="10.100.33.60",
+        user="cbeckford2",
+        password="227248309",
+        database="cbeckford_socialspread",
+        cursorclass=pymysql.cursors.DictCursor,
+        autocommit=True
+    )
 
+def get_db():
+    '''Opens a new database connection per request.'''        
+    if not hasattr(g, 'db'):
+        g.db = connect_db()
+    return g.db    
+
+@app.teardown_appcontext
+def close_db(error):
+    '''Closes the database connection at the end of request.'''    
+    if hasattr(g, 'db'):
+        g.db.close() 
 @app.route('/')
 def index():
     if flask_login.current_user.is_authenticated:
@@ -65,10 +78,10 @@ def registration_page():
         username = request.form["username"]
         password = request.form["password"]
 
-        cursor = conn.cursor()
+        cursor = get_db().cursor()
         cursor.execute(f"INSERT INTO `user` (`first_name`, `last_name`, `username`, `password`) VALUES ('{first_name}', '{last_name}', '{username}', '{password}')")
         cursor.close()
-        conn.commit()
+        get_db().commit()
         
     return render_template('register.html.jinja')
 
@@ -80,11 +93,11 @@ def login():
     if request.method == 'POST':
         username = request.form["username"]
         password = request.form["password"]
-        cursor = conn.cursor()
+        cursor = get_db().cursor()
         cursor.execute(f"SELECT * FROM `user` WHERE `username` = '{username}'")
         user = cursor.fetchone()
         cursor.close()
-        conn.commit()
+        get_db().commit()
 
         if request.form['password'] == user['password']:
             user = load_user(user['id'])
@@ -105,5 +118,11 @@ def post_feed():
 
     return flask_login.current_user
 
+@app.route('/post', methods=['POST'])
+@flask_login.login_required
+def create_post():
+    description = request.form['description']
+    user_id = flask_login.current_user.id
+    cursor = get_db().cursor()
 
- 
+    cursor.execute("INSERT INTO `posts` (`description`, `user_id`)")
